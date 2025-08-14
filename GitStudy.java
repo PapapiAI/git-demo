39137b1229b589be6ed43aaec0476a25aad3c8d9
huynh hai thien <huynhhaithien@gmail.com>
2025-08-14 09:40:47 +0700

    feat: add new feature 76546546

--- GitStudy.java
+++ GitStudy.java
@@ -4,5 +4,6 @@ public class GitStudy {
     public static void main(String[] args) {
         System.out.println("add new 5");
         System.out.println("add a new 67");
+        System.out.println("add a new 7");
     }
 }

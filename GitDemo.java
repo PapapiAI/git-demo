cebdc8559ce5f24e57b93f35ae08a279c6ebf848
huynh hai thien <huynhhaithien@gmail.com>
2025-08-14 09:10:56 +0700

    feat: add new 5

--- GitDemo.java
+++ GitDemo.java
@@ -2,6 +2,7 @@ package src.git_demo;
 
 public class GitDemo {
     public static void main(String[] args) {
-        System.out.println("Hello GitHub");
+        System.out.println("Hello GitHub1");
+        System.out.println("add line 6");
     }
 }
